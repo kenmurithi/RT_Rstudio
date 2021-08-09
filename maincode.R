1b52d5c6b6be31e41bad7000efe9ff90ec3864a5
kenmurithi <kenmurithi522@gmail.com>
2021-08-09 05:50:31 +0300

    Updated script, with the main topics 1. Introduction to R upto dataframe exploration

--- maincode.R
+++ maincode.R
@@ -10,9 +10,126 @@ cats <- data.frame(coat = c("calico", "black", "tabby"),
                    weight = c(2.1, 5.0, 3.2),
                    likes_string = c(1, 0, 1))
 cats
-## 2.Expoling the data
+typeof(cats["coat"])
+typeof(cats$coat)
+
+## 2.Exploring the data
+
 names(cats) # getting column names
+names(cats)[2] <- "weight_kg" # renaming the column names
+names(cats)
+cats
 cats$coat # getting specifically the coat column details
+
 ## 3. Other operations on columns
+
 cats$weight + 2
 paste("my cat is",cats$coat)
+
+## 4.Coercion and further data exploration
+
+a = as.numeric(cats$coat)
+b = as.numeric(cats$weight)
+a+b
+a
+b
+typeof(cats$weight)
+class(cats$weight)
+typeof(1L)
+c=1.2
+d=2
+typeof(c)
+typeof(d)
+class(c)
+class(d)
+
+## 5.factors
+
+coats <- c('tabby', 'tortoiseshell', 'tortoiseshell', 'black', 'tabby')
+coats
+str(coats) # structure of coats and we get its a character
+CATegories <- factor(coats) # creating the factors
+is.factor(CATegories)
+class(CATegories)
+levels(CATegories)
+
+## Challenge 2
+## Is there a factor in our cats data.frame?
+
+cats
+str(cats)
+is.factor(cats$coat)
+cats$coat_factor <- factor(cats$coat, levels = c("calico","black","tabby"),labels = c(1,2,3))
+cats
+is.factor(cats$coat_factor) # its a factor
+
+## 6.lists
+
+list_example <- list(1, "a", TRUE, 1+4i)
+list_example
+another_list <- list(title = "Numbers", numbers = 1:10, data = TRUE )
+another_list
+## challenge 3
+cats
+cats[1] # has indexes
+cats[[1]] # no indexes
+typeof(cats[[1]]) 
+cats["coat"]
+cats[1,1] # calcio
+cats[, 1] # all rows but column one
+
+## matrices
+# skipped this
+
+# 7.exploring data frame
+
+age <- c(2, 3, 5)
+age
+cats
+
+cats = cbind(cats,age)
+cats
+newRow <- list("tortoiseshell", 3.3, TRUE, 9)
+cats <- rbind(cats, newRow)
+
+str(cats)
+cats
+human_age = cats$age*7
+human_age
+str(human_age)
+as.factor(human_age)
+as.numeric(human_age)
+
+cats
+cats = cats[-3,]
+cats
+
+
+df <- data.frame(id = c("a", "b", "c"),
+                 x = 1:3,
+                 y = c(TRUE, TRUE, FALSE),
+                 stringsAsFactors = FALSE)
+df
+
+inf = data.frame(first_name = c("ken"),
+                 last_name = c("Mugambi"),
+                 lucky_number = c(13),
+                 stringsAsFactors = FALSE)
+inf
+
+r = list("roger", "mugambi", 10)
+inf = rbind(inf,r)
+
+inf
+
+getwd()
+
+# realistic example
+gapminder <- read.csv("./data/gapminder_data.csv", stringsAsFactors = TRUE)
+
+head(gapminder)
+tail(gapminder)
+tail(gapminder, n = 15)
+
+gapminder[sample(nrow(gapminder), 5), ]
+
